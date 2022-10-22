931539eb0c4af1b7ddbee81d9be2c66a9fe0f7a9
matheus silva <103462698+teu0226@users.noreply.github.com>
2022-10-22 13:39:51 -0300

    alteração

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div class="home center">
-      <div class="home__logo">
+    <div className="home center">
+      <div className="home__logo">
         <img src="images/logo.svg" class="responsive" alt="" />
       </div>
-      <select class="home__select-users">
+      <select className="home__select-users">
         <option>User 1</option>
         <option>User 2</option>
         <option>User 3</option>
       </select>
-      <button class="button-primary">Entrar</button>
+      <button className="button-primary">Entrar</button>
     </div>
   );
 }
