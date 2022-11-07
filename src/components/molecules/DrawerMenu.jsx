24b1433793d9d045e3707fdbe3df54566f529d7c
matheus silva <103462698+teu0226@users.noreply.github.com>
2022-11-07 14:50:37 -0300

    alteração

--- src/components/molecules/DrawerMenu.jsx
+++ src/components/molecules/DrawerMenu.jsx
@@ -1,26 +1,2 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Drawer from "@mui/material/Drawer";
-import { List, ListItem } from "@mui/material";
 
-export default function DrawerMenu(props) {
-  return (
-    <Drawer
-      anchor="right"
-      open={props.open}
-      onClose={() => props.setOpen(false)}
-    >
-      <List>
-        <ListItem>
-          <Link to="/">Home</Link>
-        </ListItem>
-        <ListItem>
-          <Link to="/users">Usuários</Link>
-        </ListItem>
-        <ListItem>
-          <Link to="/users/posts/new">Criar post</Link>
-        </ListItem>
-      </List>
-    </Drawer>
-  );
-}
+
--- src/components/templates/Default.jsx
+++ src/components/templates/Default.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import AppHeader from "../organisms/AppHeader";
 import AppFooter from "../organisms/AppFooter";
-import DrawerMenu from "../molecules/DrawerMenu";
+import DrawerMenu from "../molecules/DrawerMenu.1";
 
 export default function Default(props) {
   const [open, setOpen] = React.useState(false);
