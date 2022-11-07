053c77085fd55ff0e7739ce7b610c3ead1861231
matheus silva <103462698+teu0226@users.noreply.github.com>
2022-11-07 14:52:16 -0300

    alteração

--- src/components/molecules/PostListWrapper.jsx
+++ src/components/molecules/PostListWrapper.jsx
@@ -1,19 +1,3 @@
-import React from "react";
 
-import PostListItem from "./PostListItem";
 
-export default function PostListWrapper(props) {
-  return (
-    <div className="user-blog__posts">
-      {props.posts.map((post) => (
-        <PostListItem
-          key={post.id}
-          image={post.image}
-          title={post.title}
-          content={post.content}
-          createdAt={post.createdAt}
-        />
-      ))}
-    </div>
-  );
-}
+
